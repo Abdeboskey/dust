e23d4a54ba92f307f7ae639f165feb7af1ab4df2
Aaron Burris-DeBoskey <abdeboskey@gmail.com>
2022-04-28 17:49:45 -0600

    Update image and text

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+import logo from './assets/nu-logo.png';
 import './App.css';
 
 function App() {
@@ -6,16 +6,14 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <p>Coming Soon</p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://www.instagram.com/denverurbanskate/?hl=en"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Visit our Instagram
         </a>
       </header>
     </div>
