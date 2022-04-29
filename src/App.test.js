2b4b9682d1897f17b4f60f3717b64c668aa65f10
Aaron Burris-DeBoskey <abdeboskey@gmail.com>
2022-04-28 18:05:09 -0600

    Update tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,14 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('displays under-construction message', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const welcomeMessage = screen.getByText(/best night of the week/i);
+  expect(welcomeMessage).toBeInTheDocument();
+})
+
+test('renders instagram link', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/visit our instagram/i);
   expect(linkElement).toBeInTheDocument();
 });
