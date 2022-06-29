d5fe4f71cea24da49094253c6b51c9e3e8f86c41
Aaron Burris-DeBoskey <abdeboskey@gmail.com>
2022-06-29 17:26:57 -0600

    Change text alignment for mobile

--- src/components/Hero/index.js
+++ src/components/Hero/index.js
@@ -8,7 +8,7 @@ const Hero = () => {
         src={sunsetSkate}
         alt="Many rollerbladers skating into the sunset"
       />
-      <div className="m-4 md:absolute md:top-[12%] lg:top-[14%] md:left-12 lg:left-16">
+      <div className="text-center md:text-left m-4 md:absolute md:top-[12%] lg:top-[14%] md:left-12 lg:left-16">
         <h1 className="text-5xl md:text-5xl lg:text-6xl font-bold italic drop-shadow-md">
           Denver Urban Skate Troop
         </h1>
