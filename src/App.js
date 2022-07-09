660ee61a99f6e67cdf7f51b4a99985b8b3c45d77
Aaron Burris-DeBoskey <abdeboskey@gmail.com>
2022-07-08 21:14:24 -0600

    Create footer

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import {
 import Header from './components/Header';
 import Home from './components/Home';
 import WelcomeFAQ from './components/WelcomeFAQ';
+import Footer from './components/Footer'
 
 function App() {
   return (
@@ -23,6 +24,7 @@ function App() {
         />
         {/* <ComingSoon /> */}
       </Routes>
+      <Footer />
     </div>
   );
 }
