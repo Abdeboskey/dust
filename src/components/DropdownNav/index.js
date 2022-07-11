59166df1fd178fff82d1cf3ee4c214eb4970fafd
Aaron Burris-DeBoskey <abdeboskey@gmail.com>
2022-07-11 13:34:43 -0600

    Change links to navlinks and fix dropdown

--- src/components/DropdownNav/index.js
+++ src/components/DropdownNav/index.js
@@ -1,40 +1,30 @@
-import { Link } from "react-router-dom";
+import { NavLink } from "react-router-dom";
 
-const DropdownNav = ({ memo }) => {
+const DropdownNav = ({ memo, onClick }) => {
   const navlinkStyle = "inline-block text-white italic hover:text-gray-900 my-2";
-
 
   return (
     <div className="md:hidden relative inline-block text-left" ref={memo}>
-      {/*
-        Dropdown menu, show/hide based on menu state.
-
-        Entering: "transition ease-out duration-100"
-          From: "transform opacity-0 scale-95"
-          To: "transform opacity-100 scale-100"
-        Leaving: "transition ease-in duration-75"
-          From: "transform opacity-100 scale-100"
-          To: "transform opacity-0 scale-95"
-      */}
       <div
         className="origin-top-right absolute right-0 mt-5 p-3 w-36 rounded-md shadow-lg bg-dusteal ring-1 ring-black ring-opacity-5 focus:outline-none"
         role="menu"
         aria-orientation="vertical"
         tabIndex="-1"
       >
-        <Link to="/">
+        <NavLink to="/" onClick={onClick}>
           <span className={navlinkStyle}>HOME</span>
-        </Link>
-        <Link to="/welcome">
+        </NavLink>
+        <NavLink to="/welcome" onClick={onClick}>
           <span className={navlinkStyle}>WELCOME/FAQ</span>
-        </Link>
-        <Link to="/resources">
+        </NavLink>
+        <NavLink to="/resources" onClick={onClick}>
           <span className={navlinkStyle}>RESOURCES</span>
-        </Link>
+        </NavLink>
         <a
           href="https://denverurbanskatetroop.bigcartel.com/"
           target="_blank"
           rel="noreferrer"
+          onClick={onClick}
         >
           <span className={navlinkStyle}>SHOP</span>
         </a>
--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,5 +1,5 @@
-import { useEffect, useRef, useState } from "react";
-import { Link } from "react-router-dom";
+import { useEffect, useCallback, useRef, useState } from "react";
+import { NavLink } from "react-router-dom";
 import logo from "../../assets/nu-logo.png";
 import DropdownNav from "../DropdownNav";
 
@@ -10,47 +10,52 @@ const Header = () => {
 
   const navlinkStyle =
     "inline-block text-white italic hover:text-gray-900 mr-4";
+
+  const toggleMobileNav = useCallback(() => {
+    setIsNavOpen(isNavOpen ? false : true);
+  }, [isNavOpen]);
   
-   useEffect(() => {
-     const checkIfClickedOutside = (e) => {
-       // If the menu is open and the clicked target is not within the menu,
-       // then close the menu
-       if (isNavOpen && ref.current && !ref.current.contains(e.target)) {
-         setIsNavOpen(false);
-       }
-     };
+  useEffect(() => {
+    const checkIfClickedOutside = (e) => {
+      // If the menu is open and the clicked target is not within the menu,
+      // then close the menu
+      if (isNavOpen && e.target.classList.contains('animate-pulse')) {
+        setIsNavOpen(true);
+      } else if (isNavOpen && ref.current && !ref.current.contains(e.target)) {
+        toggleMobileNav();
+      }
+    };
 
-     document.addEventListener("mousedown", checkIfClickedOutside);
+    document.addEventListener("mousedown", checkIfClickedOutside);
 
-     return () => {
-       // Cleanup the event listener
-       document.removeEventListener("mousedown", checkIfClickedOutside);
-     };
-   }, [isNavOpen]);
-
+    return () => {
+      // Cleanup the event listener
+      document.removeEventListener("mousedown", checkIfClickedOutside);
+    };
+  }, [isNavOpen, toggleMobileNav]);
 
   return (
     <header className="bg-dusteal border-b-8 border-gray-900 sticky top-0 p-6 z-10">
       <div className="flex items-center justify-between flex-wrap">
         <div className="block">
-          <Link to="/">
+          <NavLink to="/">
             <img
               src={logo}
               alt="Denver Urban Skate Troop Logo"
               className="absolute h-16 top-2 left-2 md:h-[90px] md:top-4 md:left-4"
             />
-          </Link>
+          </NavLink>
         </div>
         <nav className="hidden md:block">
-          <Link to="/">
+          <NavLink to="/">
             <span className={navlinkStyle}>HOME</span>
-          </Link>
-          <Link to="/welcome">
+          </NavLink>
+          <NavLink to="/welcome">
             <span className={navlinkStyle}>WELCOME/FAQ</span>
-          </Link>
-          <Link to="/resources">
+          </NavLink>
+          <NavLink to="/resources">
             <span className={navlinkStyle}>RESOURCES</span>
-          </Link>
+          </NavLink>
           <a
             href="https://denverurbanskatetroop.bigcartel.com/"
             target="_blank"
@@ -61,7 +66,7 @@ const Header = () => {
         </nav>
         <div
           className="absolute right-8 flex md:hidden cursor-pointer z-30"
-          onClick={() => setIsNavOpen(!isNavOpen)}
+          onClick={toggleMobileNav}
         >
           <div className="space-y-2">
             <span className="block w-8 h-1 bg-gray-900 animate-pulse"></span>
@@ -69,7 +74,7 @@ const Header = () => {
             <span className="block w-8 h-1 bg-gray-900 animate-pulse"></span>
           </div>
         </div>
-        {isNavOpen && <DropdownNav memo={ref} />}
+        {isNavOpen && <DropdownNav memo={ref} onClick={toggleMobileNav} />}
       </div>
     </header>
   );
