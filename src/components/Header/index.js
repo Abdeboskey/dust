92bb03b3f2f2d8e2aa182686ae930cfd52e3fb9f
Geph <ginger@illinois.edu>
2023-01-14 00:25:33 -0700

    No link reference to route maps yet so we can preview it; updated copyright date but I want to state for the record I'm a fan of CC-4.0 :)

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -53,9 +53,6 @@ const Header = () => {
           <NavLink to="/welcome">
             <span className={navlinkStyle}>WELCOME/FAQ</span>
           </NavLink>
-          <NavLink to="/routemaps">
-            <span className={navlinkStyle}>ROUTE MAPS</span>
-          </NavLink>
           <NavLink to="/resources">
             <span className={navlinkStyle}>RESOURCES</span>
           </NavLink>
