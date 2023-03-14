e5a0c0a88a13187813cc7747a7e100b6394441c0
Aaron Burris-DeBoskey <abdeboskey@gmail.com>
2023-03-14 11:23:17 -0600

    Reactify Jeff's PR for routemaps

--- src/components/RouteMaps/RouteMap.js
+++ src/components/RouteMaps/RouteMap.js
@@ -1,24 +1,48 @@
-const RouteMap = ({ props }) => {
+const RouteMap = (props) => {
   const {
     name,
     length,
-    difficulty,
-    startingLocation,
+    avgSpeed,
+    rating,
+    elevation,
+    startName,
+    startUrl,
     img,
   } = props;
 
+  const getRatingEmoji = (color) => {
+    if (color === 'Green') {
+      return '🟩';
+    } else if (color === 'Blue') {
+      return '🟦';
+    } else if (color === 'Black') {
+      return '⬛';
+    }
+  }
+
+  const milesToKm = (miles) => {
+    return Math.round(miles * 1.609344);
+  }
+
+  const feetToMeters = (feet) => {
+    return Math.round(feet * 3.28084);
+  }
+
   return (
-    <div>
-      <h2 className="text-3xl font-bold m-8 mt-12">{name}</h2>
-      <p className="max-w-md sm:text-xl mx-auto">
-        <strong>Length:</strong> ~ {length} miles ( km) &nbsp;&nbsp;&nbsp;&nbsp;<strong>Difficulty:</strong> 🟦 <a href="../welcome#ratings" target="_blank">Blue</a><br />
-        Start at {startingLocation}
-      </p>
+    <div className="my-8">
+      <h3 className="text-3xl font-bold m-8">{name}</h3>
       <img
         src={img}
         alt={name + "route map"}
-        className="w-4/5 mx-auto"
+        className="w-4/5 md:w-1/3 my-6 mx-auto"
       />
+      <p className="max-w-md sm:text-xl md:text-left mx-auto">
+        <strong>Length:</strong> ~{length} miles ({milesToKm(length)} km) <br />
+        <strong>Difficulty:</strong><a href="../welcome#ratings" target="_blank"> {getRatingEmoji(rating)} {rating}</a><br />
+        <strong>Average Speed:</strong> ~{avgSpeed} mph ({milesToKm(avgSpeed)} kmh) <br />
+        <strong>Elevation Change:</strong> {elevation} ft ({feetToMeters(elevation)} m) <br />
+        <strong>Start:</strong><a href={startUrl} target="_blank" rel="noopener noreferrer"> {startName}</a>
+      </p>
     </div>
   );
 };
