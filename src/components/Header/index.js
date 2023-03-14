1242b2835fa0f5cb24020b90165a33862d35fb85
Aaron Burris-DeBoskey <abdeboskey@gmail.com>
2023-03-14 11:57:52 -0600

    Add navigation for new routes path

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -53,6 +53,9 @@ const Header = () => {
           <NavLink to="/welcome">
             <span className={navlinkStyle}>WELCOME/FAQ</span>
           </NavLink>
+          <NavLink to="/routemaps">
+            <span className={navlinkStyle}>ROUTES</span>
+          </NavLink>
           <NavLink to="/resources">
             <span className={navlinkStyle}>RESOURCES</span>
           </NavLink>
